fd7b8776462ef6f79d43664549f41507a971cef4
Brett Gilbert <BGilbert@kapow.com>
2020-01-27 00:29:05 -0600

    Cleaned up source code and fixed options page

--- actions.js
+++ actions.js
@@ -1,58 +1,38 @@
 
 (function() {
 // Runs on load	
-var globalMinify;
 var globalQuickFocus;
-var globalAddEdit;
+var globalMoveQuickFind;
+var globalMinify;
 var globalResizeProcessBuilder;
+var globalNavFavs;
+
+//var globalAddEdit;
 
 	$(function() {
+		// Loading the settings set from the Options page
 	    //console.log( "dom ready!" );
 	     chrome.storage.sync.get({
-		    minify: false,
-		    quickFocus: false,
-		    addEdit:false,
-		    resizePB:false
+	     	quickFocus: true,
+	     	moveQuickFind: true,
+		    minify: true,
+		    resizePB: true,
+		    navFavs: true
+		    //addEdit: true
 		  }, function(items) {
+		  	 globalQuickFocus = items.quickFocus;
+		  	 globalMoveQuickFind = items.moveQuickFind;
 		     globalMinify = items.minify;
-		     globalQuickFocus = items.quickFocus;
-		     globalAddEdit = items.addEdit;
 		     globalResizeProcessBuilder = items.resizePB;
+		     globalNavFavs = items.navFavs;
+		     //globalAddEdit = items.addEdit;
 		  });
 	});
 
 //*** Set focus to Quick Find filter inputs ***//
-// Waits for initial load to complete
-// Set quick find input field as focus Setup Home
-	$(document).arrive('.filter-box.input', function(){
-		var $omniQuickFind = $(this);
-		//console.log('filter found');
-		setTimeout(
-		  function() 
-		  { $omniQuickFind.focus();
-		  }, 1500);
-		//console.log("globalMinify " + globalMinify);
-		console.log("globalQuickFocus " + globalQuickFocus);
-	});
+// Waits for initial item to load then sets focus after timeout
+// Multiple Quick Find fields
 
-// Set quick find input field as focus Setup > Object Manager
-	$(document).arrive("#globalQuickfind", function() {
-    	console.log('globalQuickfind');
-    	if(globalQuickFocus == true){
-	    	var $newElem = $(this);
-	    	// Move quick find input to the left
-	    	$newElem.closest('.bRight').siblings('.bLeft').css("float","left");
-	    	$newElem.closest('.bRight').css("float","left");
-	    	$newElem.closest('.bRight').css("margin-top","33px");
-	    	$('.slds-page-header').css("padding","0.3rem 1rem");
-
-			setTimeout(
-			  function() 
-			  { 
-			  	$newElem.focus();  //set focus to quick find to start typing immediately
-			  }, 2000);
-		}
-  	});
 // Set quick find input field as focus Setup > Home
 	$(document).arrive(".filter-box.input", function() {
     	//console.log('filter-box input');
@@ -62,8 +42,21 @@ var globalResizeProcessBuilder;
 			  function() 
 			  { 
 			  	$newElem.focus();  //set focus to quick find to start typing immediately
-			  }, 2000);
+			  }, 1500);
 	  	}
+  	});
+
+// Set quick find input field as focus Setup > Object Manager
+	$(document).arrive("#globalQuickfind", function() {
+    	//console.log('globalQuickfind');
+    	if(globalQuickFocus == true){
+	    	var $newElem = $(this);
+			setTimeout(
+			  function() 
+			  { 
+			  	$newElem.focus();  //set focus to quick find to start typing immediately
+			  }, 1500);
+		}
   	});
 
 //run when clicked
@@ -73,15 +66,15 @@ var globalResizeProcessBuilder;
 	  //Let's the page update
 	  if(globalQuickFocus == true){
 		  setTimeout(
-				  function() 
-				  { //checks if the tab has focus and then applies focus to the filter depending on the tab
-				  	if($('.main-content.split').length == 0 && $('li.tabItem').hasClass('slds-has-focus')){
-					  	$('#globalQuickfind').focus();  //set focus to quick find to start typing immediately
-					}
-					else if($('.main-content.split').length == 1 && $('li.tabItem').hasClass('slds-has-focus')){
-						$('.filter-box.input').focus();
-					}
-				  }, 1000);
+			  function() 
+			  { //checks if the tab has focus and then applies focus to the filter depending on the tab
+			  	if($('.main-content.split').length == 0 && $('li.tabItem').hasClass('slds-has-focus')){
+				  	$('#globalQuickfind').focus();  //set focus to quick find to start typing immediately
+				}
+				else if($('.main-content.split').length == 1 && $('li.tabItem').hasClass('slds-has-focus')){
+					$('.filter-box.input').focus();
+				}
+			  }, 1000);
 		}
 	});
 //^^^ Set focus to Quick Find filter inputs ^^^//
@@ -91,20 +84,27 @@ var globalResizeProcessBuilder;
 //*** Custom formatting ***//
   // Move Quick find filter to the left where all the other quick find filters are
 	$(document).arrive("#globalQuickfind", function() {
-    	// 'this' refers to the newly available element
-    	if(globalMinify == true){
+    	if(globalMoveQuickFind == true){
 	    	var $newElem = $(this);
-	    	//alert("arrived");
 	    	// Move quick find input to the left
+	    	 // Object Manager Home
 	    	$newElem.closest('.bRight').siblings('.bLeft').css("float","left");
 	    	$newElem.closest('.bRight').css("float","left");
 	    	$newElem.closest('.bRight').css("margin-top","33px");
+	    	 // Object Manager > Any object detail page
+	    	$('.objectManagerVirtualRelatedListCard .slds-page-header .slds-grid').css("display", "inline-flex");
+			$('.objectManagerVirtualRelatedListCard .slds-page-header .slds-grid header').css("margin-right", "8px");
 	    }
   	});
 	
-  // Minimize the sandbox label header size
+  // Reduce the extra padding in the main gutters
+	$(document).arrive(".slds-template_default", function() {
+	  	if(globalMinify == true){
+	  		$('.slds-template_default').css("padding","0.4rem");
+		}
+	});
+  // Slim the sandbox label header size
 	$(document).arrive('.oneSystemMessage > .system-message', function() {
-    	// 'this' refers to the newly created element
     	if(globalMinify == true){
 	    	var $newElem2 = $(this);
 			$newElem2.css('padding','0');
@@ -115,57 +115,56 @@ var globalResizeProcessBuilder;
 
 //*** Resize process builder - drag ***//
 $(document).arrive('.panelContainer', function(){
-//	$(document).click(function(e) {
-	  	//console.log(e.target.nodeName);
-	  	//$('.slds-is-active.active').css("border","solid 10px red");
-	  	//Let's the page update
-	  	//console.log('fired1');
-	  	//$('.panelContainer').addClass('fired2');
+//	$(document).click(function(e) { //<-- used to find the item clicked
+  	//console.log(e.target.nodeName);
+  	//$('.slds-is-active.active').css("border","solid 10px red");
+    if(globalResizeProcessBuilder == true){
+		var isResizing = false,
+		    lastDownX = 0;
 
+		    if($('#handle').length){
+		    	//console.log("handle exists");
+		    }
+		    else{
+		    	$('.panelContainer').css({"position":"absolute","right":"0","height":"calc(100% - 100px)"});
+		    	$('.panelContainer').prepend('<div id="handle" style="position:absolute; border-left:solid 1px #cfcfcf; border-right:solid 1px #cfcfcf; height:100%; width: 8px; cursor: col-resize; z-index:100;"><div style="position:absolute;top: 46%;border: solid 1px #cfcfcf;width: 4px;height:35px;left: 1px;background: #cfcfcf;"</div></div>');
+		    	$('.processuicommonPanel .panelBody').css("width","100%");
+		    }
 
-						var isResizing = false,
-						    lastDownX = 0;
+		    var container = $('.wrapper'),
+		        left = $('.ruleContainer'),
+		        right = $('.panelContainer'),
+		        handle = $('#handle');
 
-						    if($('#handle').length){
-						    	//console.log("handle exists");
-						    }
-						    else{
-						    	$('.panelContainer').css({"position":"absolute","right":"0","height":"calc(100% - 100px)"});
-						    	$('.panelContainer').prepend('<div id="handle" style="position:absolute; border-left:solid 1px #cfcfcf; border-right:solid 1px #cfcfcf; height:100%; width: 8px; cursor: col-resize; z-index:100;"><div style="position:absolute;top: 46%;border: solid 1px #cfcfcf;width: 4px;height:35px;left: 1px;background: #cfcfcf;"</div></div>');
-						    	$('.processuicommonPanel .panelBody').css("width","100%");
-						    }
+		    handle.on('mousedown', function (e) {
+		        isResizing = true;
+		        lastDownX = e.clientX;
+		    });
 
-						    var container = $('.wrapper'),
-						        left = $('.ruleContainer'),
-						        right = $('.panelContainer'),
-						        handle = $('#handle');
+		    $(document).on('mousemove', function (e) {
+		        // we don't want to do anything if we aren't resizing.
+		        if (!isResizing) 
+		            return;
 
-						    handle.on('mousedown', function (e) {
-						        isResizing = true;
-						        lastDownX = e.clientX;
-						    });
+		        var offsetRight = container.width() - (e.clientX - container.offset().left);
 
-						    $(document).on('mousemove', function (e) {
-						        // we don't want to do anything if we aren't resizing.
-						        if (!isResizing) 
-						            return;
-
-						        var offsetRight = container.width() - (e.clientX - container.offset().left);
-
-						        left.css('right', offsetRight);
-						        right.css('width', offsetRight);
-						    }).on('mouseup', function (e) {
-						        // stop resizing
-						        isResizing = false;
-						    });
-	});
+		        left.css('right', offsetRight);
+		        right.css('width', offsetRight);
+		    }).on('mouseup', function (e) {
+		        // stop resizing
+	        	isResizing = false;
+	   		});
+	}
+});
 
 //^^^ Resize process builder - drag ^^^//
 
 //*** View full API name in process builder modal popup ***//
 	$(document).arrive('.summaryRenderer.processuicommonSummaryRenderer.processuicommonTraverserModalBody', function(){
-		$(this).css({"width":"auto"});
-		$('.processuicommonSummaryRenderer .selectedText span').css({"white-space": "normal","overflow-wrap": "break-word"});
+		if(globalResizeProcessBuilder == true){
+			$(this).css({"width":"auto"});
+			$('.processuicommonSummaryRenderer .selectedText span').css({"white-space": "normal","overflow-wrap": "break-word"});
+		}
 	})
 //^^^ View full API name in process builder modal popup ^^^//
 
@@ -198,30 +197,36 @@ $(document).arrive('.panelContainer', function(){
 	    })
 	}
 	$(document).arrive('.panelContainer .processuicommonActionPanel .panelBodyContent td:nth-child(3) label', function(){
-		if($(this).siblings(".copyTextButton").length){
-	    	//console.log("Copy Text Button exists");
-	    	$(this).siblings(".copyTextButton").remove();
-	    }
-		$(this).after('<div class="copyTextButton" style="display:block; font-size:xx-small; cursor:pointer; color:grey;">Copy Text</div>');
-		RefreshEventListener();
+		if(globalResizeProcessBuilder == true){
+			if($(this).siblings(".copyTextButton").length){
+		    	//console.log("Copy Text Button exists");
+		    	$(this).siblings(".copyTextButton").remove();
+		    }
+			$(this).after('<div class="copyTextButton" style="display:block; font-size:xx-small; cursor:pointer; color:grey;">Copy Text</div>');
+			RefreshEventListener();
+		}
 	})
 	$(document).arrive('.panelContainer .processuicommonOutcomePanel .panelBodyContent td:nth-child(5) label', function(){
-		if($(this).siblings(".copyTextButton").length){
-	    	//console.log("Copy Text Button exists");
-	    	$(this).siblings(".copyTextButton").remove();
-	    }
-		$(this).after('<div class="copyTextButton" style="display:block; font-size:xx-small; cursor:pointer; color:grey;">Copy Text</div>');
-		RefreshEventListener();
+		if(globalResizeProcessBuilder == true){
+			if($(this).siblings(".copyTextButton").length){
+		    	//console.log("Copy Text Button exists");
+		    	$(this).siblings(".copyTextButton").remove();
+		    }
+			$(this).after('<div class="copyTextButton" style="display:block; font-size:xx-small; cursor:pointer; color:grey;">Copy Text</div>');
+			RefreshEventListener();
+		}
 	})
 //^^^ Copy or view formula code of Active Process builder ^^^//
 
 //*** Setup Nav Rail Favorites ***//
 	$(document).arrive('.tree-filter.onesetupNavTreeFilter', function(){
-		//console.log("setup loaded");
-		$(this).after('<style tyle=text/css>.favList{margin-bottom:.5rem; } .favList a:hover{background: var(--lwc-colorBackgroundRowHover,rgb(243, 242, 242));}</style><div class="favList" style="display:block;"><h4 class="slds-text-title_caps section-header" style="padding: 0 0 0 .75rem;">Favorites Quick Menu</h4><ul></ul></div>');
-		$(".favList ul").append('<li><a href="/one/one.app#/setup/CompanyProfileInfo/home" style="display:block;padding:.25rem 0 .25rem .75rem;text-decoration: none;color: var(--lwc-colorTextDefault,rgb(8, 7, 7));">Company Information</a></li>');
-		$(".favList ul").append('<li><a href="/one/one.app#/setup/ProcessAutomation/home" style="display:block;padding:.25rem 0 .25rem .75rem;text-decoration: none;color: var(--lwc-colorTextDefault,rgb(8, 7, 7));">Process Builder</a></li>');
-		$(".favList ul").append('<li><a href="/one/one.app#/setup/ManageUsers/home" style="display:block;padding:.25rem 0 .25rem .75rem;text-decoration: none;color: var(--lwc-colorTextDefault,rgb(8, 7, 7));">Users</a></li>');
+		if(globalNavFavs == true){
+			//console.log("setup loaded");
+			$(this).after('<style tyle=text/css>.favList{margin-bottom:.5rem; } .favList a:hover{background: var(--lwc-colorBackgroundRowHover,rgb(243, 242, 242));}</style><div class="favList" style="display:block;"><h4 class="slds-text-title_caps section-header" style="padding: 0 0 0 .75rem;">Favorites Quick Menu</h4><ul></ul></div>');
+			$(".favList ul").append('<li><a href="/one/one.app#/setup/CompanyProfileInfo/home" style="display:block;padding:.25rem 0 .25rem .75rem;text-decoration: none;color: var(--lwc-colorTextDefault,rgb(8, 7, 7));">Company Information</a></li>');
+			$(".favList ul").append('<li><a href="/one/one.app#/setup/ProcessAutomation/home" style="display:block;padding:.25rem 0 .25rem .75rem;text-decoration: none;color: var(--lwc-colorTextDefault,rgb(8, 7, 7));">Process Builder</a></li>');
+			$(".favList ul").append('<li><a href="/one/one.app#/setup/ManageUsers/home" style="display:block;padding:.25rem 0 .25rem .75rem;text-decoration: none;color: var(--lwc-colorTextDefault,rgb(8, 7, 7));">Users</a></li>');
+		}
 	})
 
 //^^^ Setup Nav Rail Favorites ^^^//
@@ -274,6 +279,7 @@ $(document).arrive('.panelContainer', function(){
 
 // Add edit link instead of menu dropdown
 //*** Add quick Edit ***// 
+// CURRENTLY DOES NOT WORK 
 /*
     $(document).arrive('section.related-list-card', function(){
     	if(globalAddEdit == true){
--- options.js
+++ options.js
@@ -2,13 +2,19 @@
 // Saves options to chrome.storage
 //https://developer.chrome.com/extensions/storage#type-StorageArea
 function save_options() {
-  var minify = document.getElementById('minify').checked;
   var quickFocus = document.getElementById('quickFocus').checked;
-  var addEdit = document.getElementById('addEdit').checked;
+  var moveQuickFind = document.getElementById('moveQuickFind').checked;
+  var minify = document.getElementById('minify').checked;
+  var resizePB = document.getElementById('resizePB').checked;
+  var navFavs = document.getElementById('navFavs').checked;
+  //var addEdit = document.getElementById('addEdit').checked;
   chrome.storage.sync.set({
-    minify: minify,
     quickFocus: quickFocus,
-    addEdit: addEdit
+    moveQuickFind: moveQuickFind,
+    minify: minify,
+    resizePB: resizePB,
+    navFavs: navFavs
+    //addEdit: addEdit
   }, function() {
     // Update status to let user know options were saved.
     var status = document.getElementById('status');
@@ -24,15 +30,20 @@ function save_options() {
 function restore_options() {
   // Use default values
   chrome.storage.sync.get({
-    minify: true,
     quickFocus: true,
-    addEdit: false
+    moveQuickFind: true,
+    minify: true,
+    resizePB: true,
+    navFavs: true
+    //addEdit: false
   }, function(items) {
-    document.getElementById('minify').checked = items.minify;
     document.getElementById('quickFocus').checked = items.quickFocus;
-    document.getElementById('addEdit').checked = items.addEdit;
+    document.getElementById('moveQuickFind').checked = items.moveQuickFind;
+    document.getElementById('minify').checked = items.minify;
+    document.getElementById('resizePB').checked = items.resizePB;
+    document.getElementById('navFavs').checked = items.navFavs;
+    //document.getElementById('addEdit').checked = items.addEdit;
   });
 }
 document.addEventListener('DOMContentLoaded', restore_options);
 document.getElementById('save').addEventListener('click',save_options);
-
